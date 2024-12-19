f31ff9d7554fa15efbfcc09ccf8a03c8eba9d58b
Taalaibekovi4 <rasultaalaibekovi4@gmail.com>
2024-12-19 17:16:41 +0600

    44
    44

--- myshop/urls.py
+++ myshop/urls.py
@@ -40,6 +40,8 @@ urlpatterns += [
 urlpatterns += [
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 urlpatterns += [
